9b37418486932f5b09cb4f8943790333ce00c5b8
Ifeoluwa <omolekeifeoluwa2005@gmail.com>
2024-08-19 15:01:11 +0100

    Trial of PHP

---
+++ contact-thanks.php
@@ -0,0 +1,15 @@
+<?php 
+$page_title="Contact Ife";
+$section= "contact";
+include("include/header.php");
+
+?>
+
+    <div class="section page">
+        <div class="wrapper">
+            <h1> Contact</h1>
+            <p>Thanks for the Email! I&rsquo;ll be in touch shortly</p>
+        </div>
+    </div>
+
+<?php include('include/footer.php') ?>; 
---
+++ shirts.php
@@ -0,0 +1,10 @@
+<?php 
+$page_title ="'Ife's full catalogue of shirt'";
+$section ="shirts";
+include('include/header.php'); ?>
+
+    <div class="section page">
+        <h1>Ife&rsquo;s Full Catalog Of Shirts</h1>
+    </div>
+
+<?php include('include/footer.php'); ?> 
