450dfb85a75def30baf18cca7d8d433278831169
Ifeoluwa <omolekeifeoluwa2005@gmail.com>
2024-08-21 12:24:37 +0100

    New changes made to this basic php project

--- contact.php
+++ contact.php
@@ -1,55 +1,62 @@
 <?php
 
-$name=$_POST["name"];
-$email=$_POST["email"];
-$message=$_POST["message"];
-$email_body="";
-$email_body=$email_body. "Name:". $name."\n";
-$email_body=$email_body. "Email:".$email."\n";
-$email_body=$email_body."Message:".$message."\n";
-// todoSend email
+if ($_SERVER["REQUEST_METHOD"]=="POST"){
+    $name=$_POST["name"];
+    $email=$_POST["email"];
+    $message=$_POST["message"];
+    $email_body="";
+    $email_body=$email_body. "Name:". $name."\n";
+    $email_body=$email_body. "Email:".$email."\n";
+    $email_body=$email_body."Message:".$message."\n";
+    // todoSend email
 
-header("Location:contact-thanks.php");
-
+    header("Location:contact-thanks.php?status=thanks");
+    exit;
+}
 ?> 
 <?php 
 $page_title="Contact Ife";
 $section="contact";
-include('include/header.php'); ?>
+include( 'include/header.php'); ?>
 
     <div class="section page">
         <div class="wrapper">
             <h1>Contact</h1>
-            <p>I&rsquo;d love to hear from you! Complete the form to send me an email</p>
-            <form method="post"n action="contact.php">
-                <table>
-                    <tr>   
-                        <th>
-                            <label for="name">Name</label>
-                        </th>
-                        <td>
-                            <input type="text" name="name" id="name">
-                        </td>
-                    </tr>
-                    <tr>   
-                        <th>
-                            <label for="email">Email</label>
-                        </th>
-                        <td>
-                            <input type="text" name="email" id="email">
-                        </td>
-                    </tr>
-                    <tr>   
-                        <th>
-                            <label for="message">Message</label>
-                        </th>
-                        <td>
-                            <textarea name="message" id="message"></textarea>
-                        </td>
-                    </tr>
-                </table>
-                <input type="submit" value="Send"> 
-            </form> 
+            <?php if (isset($_GET["status"]) and $_GET["status"]=="thanks"){?>
+                 <p>Thanks for the Email! I&rsquo;ll be in touch shortly</p>
+             <?php } else{?>
+
+                <p>I&rsquo;d love to hear from you! Complete the form to send me an email</p>
+                <form method="post"n action="contact.php">
+                    <table>
+                        <tr>   
+                            <th>
+                                <label for="name">Name</label>
+                            </th>
+                            <td>
+                                <input type="text" name="name" id="name">
+                            </td>
+                        </tr>
+                        <tr>   
+                            <th>
+                                <label for="email">Email</label>
+                            </th>
+                            <td>
+                                <input type="text" name="email" id="email">
+                            </td>
+                        </tr>
+                        <tr>   
+                            <th>
+                                <label for="message">Message</label>
+                            </th>
+                            <td>
+                                <textarea name="message" id="message"></textarea>
+                            </td>
+                        </tr>
+                    </table>
+                    <input type="submit" value="Send"> 
+                </form> 
+            <?php } ?>
         </div>  
     </div>
 
--- shirts.php
+++ shirts.php
@@ -1,10 +1,68 @@
 <?php 
+$products= array();
+$products[101]=array(
+    "name"=>"Logo Shirt,Red",
+    "price"=>18,
+    "img"=>"img/shirts/shirt-101.jpg",
+);
+$products[102]=array(
+    "name"=>"Mike the frog,Black",
+    "img"=>"img/shirts/shirt-102.jpg",
+    "price"=> 0,
+
+);
+$products[103]=array(
+    "name"=>"Mike the Frog Shirt,Blue",
+    "img"=>"img/shirts/shirt-103.jpg",
+    "price"=> 0,
+);
+$products[104]=array(
+    "name"=>"Logo Shirt,Green",
+    "img"=>"img/shirts/shirt-104.jpg",
+    "price"=>0,
+);
+$products[105]=array(
+    "name"=> "",
+    "img"=>"img/shirts/shirt-105.jpg",
+    "price"=>0,
+);
+$products[106]=array(
+    "name"=>"Logo Shirt, Gray",
+    "img"=> "img/shirts/shirt-106.jpg",
+    "price"=>20,
+);
+$products[107]=array(
+    "name"=> "Logo Shirt,Turquoise",
+    "img"=>"img/shirts/shirt-107.jpg",
+    "price"=> 20,
+);
+$products[108]=array(
+    "name"=> "Logo Shirt,Orange",
+    "img"=>"img/shirts/shirt-108.jpg",
+    "price"=> 25,
+);
+?>
+
+<?php 
 $page_title ="'Ife's full catalogue of shirt'";
 $section ="shirts";
 include('include/header.php'); ?>
 
-    <div class="section page">
-        <h1>Ife&rsquo;s Full Catalog Of Shirts</h1>
+    <div class="section shirts page">
+        <div class="wrapper">
+            <h1>Ife&rsquo;s Full Catalog Of Shirts</h1>
+            <ul class="products">
+                <?php foreach($products as $product){
+                        echo "<li>";
+                        echo "<a href='#'>";
+                        echo '<img src="'.$product["img"].'" alt="'.$product["name"].'">';
+                        echo "<p>View Details</p>";                    
+                        echo "</a>";
+                        echo "</li>";
+                    }
+                ?>
+            </ul>
+        </div>
     </div>
 
 <?php include('include/footer.php'); ?> 
